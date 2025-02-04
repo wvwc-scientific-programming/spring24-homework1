ab3d37de2d3d9751e32751062326a69bdae7acca
j-oldroyd <oldroyd.j@wvwc.edu>
2025-02-04 11:28:19 -0500

    Add files via upload

---
+++ homework1.m
@@ -0,0 +1,73 @@
+%% Homework 1
+% Due January 29, 2025
+
+% This file is an example of a MATLAB script. Such files contain MATLAB
+% code, and we'll see later how to actually run script files in MATLAB. For
+% now, just type your answers in the appropriate locations.
+
+% Each problem is separated into its own section by using %%. Code
+% contained within a section can be run separately from code in other
+% sections by using the "Run Section" button in the Editor menu or by
+% placing your cursor within a section and hitting CTRL+ENTER on your
+% keyboard. All code for a given problem should be placed within the
+% appropriate section.
+
+% As an example, run this section to clean your Workspace and Command
+% Window. When you do so, only the lines of code in the section that are
+% not commented out will be executed.
+clc, clear
+
+%% Problem 1
+% Suppose that I use the following code to create a variable called
+% "number" that has the value 10. What is wrong with the code below?
+10 = number
+
+%% Answer
+number = 10;
+%% Problem 2
+% Complete Exercise 9 in Chapter 1.
+
+%% Answer
+
+%% Problem 3
+% Complete Exercise 13 in Chapter 1.
+% HINT: Read about the "rand" command in Section 1.4 of the textbook.
+
+%% Answer
+
+%% Problem 4
+% Complete Exercise 16 in Chapter 1.
+
+%% Answer
+
+%% Problem 5
+% Complete Exercise 18 in Chapter 1.
+
+%% Answer
+
+%% Problem 6
+% Complete Exercise 29 in Chapter 1. HINT: See Section 1.8 of the textbook
+% to find the appropriate command for computing a square root.
+
+%% Answer
+
+%% Problem 7
+% Complete Exercise 32 in Chapter 1.
+
+%% Answer
+
+%% Problem 8
+% Complete Exercise 33 in Chapter 1.
+
+%% Answer
+
+%% Problem 9
+% Read the subsection "Assessing Classification Model Performance" on page 
+% 32, and then complete Exercise 36 in Chapter 1.
+
+%% Answer
+
+%% Problem 10
+% Complete Exercise 37 in Chapter 1.
+
+%% Answer
